849e6640b7ed6990b820f4a756aa1fbccbaefef2
klode <harley889911@gmail.com>
2025-10-01 18:16:51 +0000

    Delete hypr directory

--- hypr/hyprpaper/reload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-hyprDir=$HOME/.config/hypr # hypr directory
-
-hyprctl hyprpaper unload all # unload wallpaper
-killall auto.sh              # kill auto.sh
-
-nohup $hyprDir/hyprpaper/load.sh & # load wallpaper
